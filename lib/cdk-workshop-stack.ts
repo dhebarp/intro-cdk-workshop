4ae9f8210e5d809affc16fff19eec45de26d943f
Paritosh Dhebar <paritosh.dhebar@gmail.com>
2021-04-23 12:39:50 +1000

    additonal files commit

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,18 +1,31 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigateway'
+import { HitCounter } from "./hitcounter";
+import { TableViewer } from 'cdk-dynamo-table-viewer'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props); //constructor (like classes)
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X, // execution environment
+      code: lambda.Code.fromAsset('lambda'), //code lambda directory
+      handler: 'hello.handler' // fil is hello, function is handler
+    })
+
+    const helloWithinCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
     });
+// defines an API Gateway REST API resource backed by our "hello" function.
+   new apigw.LambdaRestApi(this, 'Endpoint', {
+     handler: helloWithinCounter.handler
+   })
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithinCounter.table,
+      sortBy: 'hits'
+    })
   }
 }
